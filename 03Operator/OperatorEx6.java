f03f7ceb4043697f2969bf215ab8719aa4b765b8
k1mjunyoung <74641191+k1mjunyoung@users.noreply.github.com>
2022-09-29 23:34:04 +0900

    3.2

---
+++ 03Operator/OperatorEx6.java
@@ -0,0 +1,8 @@
+class OperatorEx6 {
+    public static void main(String args[]) {
+        byte a = 10;
+        byte b = 30;
+        byte c = a + b; // 컴파일 에러, 형변환 필요
+        System.out.println(c);
+    }
+}
